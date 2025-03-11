abda80f0215897f4ef52c55192cbe5a2400c8a65
esaah00 <boyeayomidee@gmail.com>
2025-03-11 14:03:25 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: { enabled: true },
-      manifest: {
-        id: "/",
-        name: "WeatherApp",
-        short_name: "WeatherA",
-        description: "Latest weather update",
-        theme_color: "#646cff",
-        icons: [
-          {
-            src: "weather.jpg",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "weather.jpg",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 2029,
   },
